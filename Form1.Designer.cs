662ff725a873d7a89327e873c433e00f3303d7c2
Aniux2807 <79299383+Aniux2807@users.noreply.github.com>
2021-03-19 13:23:35 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace parcial1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 739);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Registrar ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Carnet:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Codigo del Libro:";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(43, 153);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fecha Inicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fecha de Devolucion";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Location = new System.Drawing.Point(43, 437);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(354, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(708, 266);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(669, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = " Libros Prestados";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(636, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(325, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Libros no devueltos a la fecha";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(643, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(239, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Libros pendientes de Devolucion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 876);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.monthCalendar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parcial1
+{
+    public partial class Form1 : Form
+    {
+        List<alquiler> lista1 = new List<alquiler>();
+        
+        List<estudante> lista2 = new List<estudante>();
+        List<libroo> lista3 = new List<libroo>();
+        List<lista> lista4 = new List<lista>();
+        DateTime fecha = DateTime.Now;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LeerArquilados();
+            LeerLibros();
+            LeerAlumnos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            alquiler presTemp = new alquiler();
+            presTemp.Carnet_del_Alumno = Int32.Parse(textBox1.Text);
+            presTemp.Codigo_Libro = Int32.Parse(textBox2.Text);
+            presTemp.Fecha_Prestamo = monthCalendar1.SelectionStart;
+            presTemp.Fecha_Devolucion = monthCalendar2.SelectionStart;
+           lista1.Add(presTemp);
+            FileStream stream = new FileStream("prestamos.txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            foreach (var p in lista1)
+            {
+                writer.WriteLine(p.Carnet_del_Alumno);
+                writer.WriteLine(p.Codigo_Libro);
+                writer.WriteLine(p.Fecha_Prestamo);
+                writer.WriteLine(p.Fecha_Devolucion);
+            }
+            writer.Close();
+            
+
+        }
+        private void LeerArquilados()
+        {
+            FileStream stream = new FileStream("arquiler.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                alquiler prestamoTemp = new alquiler();
+
+                prestamoTemp.Carnet_del_Alumno = Int32.Parse(reader.ReadLine());
+                prestamoTemp.Codigo_Libro = Int32.Parse(reader.ReadLine());
+                prestamoTemp.Fecha_Prestamo = Convert.ToDateTime(reader.ReadLine());
+                prestamoTemp.Fecha_Devolucion= Convert.ToDateTime(reader.ReadLine());
+                lista1.Add(prestamoTemp);
+            }
+            reader.Close();
+        }
+        private void LeerLibros()
+           
+        {
+            FileStream stream = new FileStream("libros.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                libroo libroTemp = new libroo();
+
+              libroTemp.Codigo = Int32.Parse(reader.ReadLine());
+                libroTemp.Titulo = reader.ReadLine();
+                libroTemp.Autor= reader.ReadLine();
+              libroTemp.Año= Int32.Parse(reader.ReadLine());
+               
+                lista3.Add(libroTemp);
+            }
+            reader.Close();
+        }
+        private void LeerAlumnos()
+            //Lee y agrega elementos a la lista alumnos
+        {
+            FileStream stream = new FileStream("alumnos.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                estudante esTemp = new estudante();
+
+                esTemp.Carnet = Int32.Parse(reader.ReadLine());
+              esTemp.Nombre= reader.ReadLine();
+                esTemp.Direccion = reader.ReadLine();
+               
+
+                lista2.Add(esTemp);
+            }
+            reader.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lista3.Count; i++)
+            {
+                for (int j = 0; j < lista1.Count; j++)
+                {
+                    if (lista3[i].Codigo == lista1[j].Codigo_Libro)
+                    {
+                        lista datos = new lista();
+                        datos.Nombre = lista2[i].Nombre;
+                        datos.Titulo = lista3[j].Titulo;
+                        datos.Fecha_Prestamo = lista1[i].Fecha_Prestamo;
+                        datos.Fecha_Devolucion = lista1[i].Fecha_Devolucion;
+                    
+
+                        lista4.Add(datos);
+                    }
+                }
+
+
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = lista4;
+            dataGridView1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //conteo de los libros no devueltos
+            int cuenta = 0;
+
+        
+            //Recorre toda la lista de prestamos
+            for (int i = 0; i < lista1.Count(); i++)
+            {
+                //compara la fecha actual a la fecha de devolucion si la es mayor
+                //significa que el libro no a sido devuelto
+                
+                if (lista1[i].Fecha_Devolucion > fecha)
+                {
+                    cuenta = cuenta + 1;
+                }
+            }
+
+            label5.Text = "los libros pendientes de devolucion son: " +  cuenta;
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
